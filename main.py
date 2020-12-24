69b4c1d73108ca7c3c15f272d67bff02fc612a8c
artem <arta159>
2020-12-24 15:47:02 +0400

    начало

--- main.py
+++ main.py
@@ -1,2 +1,43 @@
-print('Hello')
-print('Hello')
+import os
+import sys
+import pygame
+
+
+WIDTH = 400
+HEIGHT = 300
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+
+
+def load_image(name, color_key=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        pass
+    screen.fill(pygame.Color(0, 0, 0))
+    pygame.display.flip()
+    clock.tick(60)
+
+terminate()
